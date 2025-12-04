4dbaf11e850837aed5644033d9f88d2b5b06b6ce
jujulioed <julioeduardon9@hotmail.com>
2025-12-03 23:13:53 -0300

    Secure the application
    Signed-off-by: jujulioed <julioeduardon9@hotmail.com>

--- src/main/java/com/jujulioed/securityjava/configuration/MvcConfig.java
+++ src/main/java/com/jujulioed/securityjava/configuration/MvcConfig.java
@@ -1,4 +1,4 @@
-package com.jujulioed.securityjava.config;
+package com.jujulioed.securityjava.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
