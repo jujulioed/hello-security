1fc8d29cf65a1f8571ff1e3b7c20dd2fc2a1e501
jujulioed <julioeduardon9@hotmail.com>
2025-12-03 22:13:48 -0300

    Initializing project

---
+++ src/main/java/com/jujulioed/securityjava/SecurityjavaApplication.java
@@ -0,0 +1,13 @@
+package com.jujulioed.securityjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jujulioed/securityjava/SecurityjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jujulioed.securityjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
